2ddcc28f8742d595d51c64788609da45af459e20
jspmic <micaeljaspe@gmail.com>
2025-06-26 12:00:51 +0200

    feat: modified ndkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,7 @@ plugins {
 android {
     namespace = "com.example.medico"
     compileSdk = flutter.compileSdkVersion
-	ndkVersion = "27.0.12077973"
-    // ndkVersion = flutter.ndkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11

370f27ea485059a0de3e8f1994516dce374e6ae1
jspmic <micaeljaspe@gmail.com>
2025-06-30 10:10:44 +0200

    feat: added bottomBar and on-site navigation

--- lib/rdvScreen.dart
+++ lib/rdvScreen.dart
@@ -2,78 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:medico/anotherScreen.dart' as anotherScreen;
 import 'package:medico/myselfScreen.dart' as myselfScreen;
 import 'package:medico/custom_widgets.dart';
+import 'package:medico/api/utilisateur_model.dart';
 
 late Color? background;
+UtilisateurModel utilisateur = UtilisateurModel();
 class ScreenTransition {
   late Color? backgroundColor;
-  ScreenTransition({required this.backgroundColor}) {
+  UtilisateurModel user;
+  ScreenTransition({required this.backgroundColor, required this.user}) {
     background = backgroundColor;
+	utilisateur = user;
   }
 }
 
-class Rdv extends StatelessWidget {
-  const Rdv({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-	  backgroundColor: background,
-      appBar: AppBar(
-		  backgroundColor: Colors.transparent,
-		  title: Align(
-			  alignment: Alignment.topRight,
-			  child: IconButton(onPressed: (){ Navigator.pop(context); }, icon: Icon(Icons.arrow_back, color: Colors.red)), // back button
-		  ), // Align
-		  leading: Builder(
-			  builder: (context) {
-				return Align(
-				alignment: Alignment.topCenter,
-				child: IconButton(
-				  color: Colors.red,
-				  icon: const Icon(Icons.menu),
-				  onPressed: () {
-					Scaffold.of(context).openDrawer();
-          },
-        ) // IconButton
-		); // Align
-      },
-    ),
-      ), // AppBar
-	  drawer: Drawer(
-		  backgroundColor: background,
-		  elevation: 0,
-		  child: ListView(
-			children: [
-				DrawerHeader(
-				  decoration: BoxDecoration(color: getSubtitlesColor(background)),
-				  child: Text('Menu', style: TextStyle(color: getColor(background))),
-            ),
-			]
-		  ) // ListView
-	  ),
-	  body: RdvPage(),
-	  );
-  }
+class DrawerOptions {
+	final String label;
+	final Widget icon;
+	final Widget selectedIcon;
+	const DrawerOptions({required this.label, required this.icon, required this.selectedIcon});
 }
 
-class RdvPage extends StatefulWidget {
-  const RdvPage({super.key});
+const List<DrawerOptions> options = [
+	DrawerOptions(label: "Accueil", icon: Icon(Icons.home_outlined, color: Colors.red), selectedIcon: Icon(Icons.home, color: Colors.red)),
+	DrawerOptions(label: "Profil", icon: Icon(Icons.person_outlined, color: Colors.red), selectedIcon: Icon(Icons.person, color: Colors.red)),
+	DrawerOptions(label: "Se déconnecter", icon: Icon(Icons.logout_outlined, color: Colors.red), selectedIcon: Icon(Icons.logout, color: Colors.red)),
+];
 
-  @override
-  State<RdvPage> createState() => _RdvPageState();
-}
-
-class _RdvPageState extends State<RdvPage> {
-
-  @override
-  Widget build(BuildContext context) {
-	  return Center(
+Widget index0(BuildContext context) {
+  return SingleChildScrollView(
+	  child: Center(
 			  child: Padding(
-			  padding: EdgeInsets.all(MediaQuery.of(context).size.width/8),
+			  padding: EdgeInsets.all(MediaQuery.of(context).size.width/10),
 			  child:
 				  Column(
+					  mainAxisAlignment: MainAxisAlignment.center,
+					  crossAxisAlignment: CrossAxisAlignment.center,
 					  children: [
-						  SizedBox(height: MediaQuery.of(context).size.height/15),
+						  SizedBox(height: MediaQuery.of(context).size.height/5),
 						  Card(
 							elevation: 9,
 							color: background,
@@ -84,7 +49,7 @@ class _RdvPageState extends State<RdvPage> {
 								  onTap: (){
 									  Navigator.push(context, 
 										MaterialPageRoute(builder: (context) {
-											myselfScreen.ScreenTransition(backgroundColor: background);
+											myselfScreen.ScreenTransition(backgroundColor: background, user: utilisateur);
 											return myselfScreen.Myself();
 										}),
 										);
@@ -111,7 +76,7 @@ class _RdvPageState extends State<RdvPage> {
 								  onTap: (){
 									  Navigator.push(context, 
 										MaterialPageRoute(builder: (context) {
-											anotherScreen.ScreenTransition(backgroundColor: background);
+											anotherScreen.ScreenTransition(backgroundColor: background, user: utilisateur);
 											return anotherScreen.Another();
 										}),
 										);
@@ -128,6 +93,112 @@ class _RdvPageState extends State<RdvPage> {
 						]
 					) // Column
 				) // Padding
-			); // Center
+			) // Center
+		); // SingleChildScrollView
+}
+
+Widget index1(BuildContext context) {
+  TextEditingController email = TextEditingController();
+  TextEditingController numeroTelephone = TextEditingController();
+  TextEditingController dateNaissance = TextEditingController();
+  DateTime date = utilisateur.dateNaissance!;
+  email.text = utilisateur.email!;
+  numeroTelephone.text = utilisateur.numeroTelephone!;
+  dateNaissance.text = "${date.day}/${date.month}/${date.year}";
+  return Center(
+		  child: Padding(
+		  padding: EdgeInsets.all(MediaQuery.of(context).size.width/10),
+		  child:
+			  Column(
+				  mainAxisAlignment: MainAxisAlignment.center,
+				  crossAxisAlignment: CrossAxisAlignment.center,
+				  children: [
+					Row(
+						children: [
+							Text("Email: ", style: TextStyle(color: getColor(background))),
+							SizedBox(
+								width: MediaQuery.of(context).size.width/1.8,
+								child: TextField(
+									controller: email,
+									style: TextStyle(color: getSubtitlesColor(background)),
+									decoration: InputDecoration(
+										border: InputBorder.none
+									),
+								), // TextField
+							),
+					]), // Row
+					Row(
+						children: [
+							Text("Numéro: ", style: TextStyle(color: getColor(background))),
+							SizedBox(
+								width: MediaQuery.of(context).size.width/3,
+								child: TextField(
+									controller: numeroTelephone,
+									style: TextStyle(color: getSubtitlesColor(background)),
+									decoration: InputDecoration(
+										border: InputBorder.none
+									),
+								), // TextField
+							), // SizedBox
+					]), // Row
+					Row(
+						children: [
+							Text("Date de naissance: ", style: TextStyle(color: getColor(background))),
+							SizedBox(
+								width: MediaQuery.of(context).size.width/3,
+								child: TextField(
+									enabled: false,
+									controller: dateNaissance,
+									style: TextStyle(color: getSubtitlesColor(background)),
+									decoration: InputDecoration(
+										border: InputBorder.none
+									),
+								), // TextField
+							),
+					]), // Row
+				  ]
+				) // Column
+			) // Padding
+		); // Center
+}
+
+class RdvPage extends StatefulWidget {
+  const RdvPage({super.key});
+
+  @override
+  State<RdvPage> createState() => _RdvPageState();
+}
+
+class _RdvPageState extends State<RdvPage> {
+  int screenIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+	  backgroundColor: background,
+	  bottomNavigationBar: NavigationBar(
+		  backgroundColor: background,
+		  labelTextStyle: WidgetStatePropertyAll<TextStyle>(TextStyle(color: getColor(background))),
+		  elevation: 10,
+		  selectedIndex: screenIndex,
+		  onDestinationSelected: (int index) {
+		  	setState(() {
+			  if (index == 2) {
+			  	Navigator.pop(context);
+			  }
+		  	  screenIndex = index;
+		  	});
+		  },
+		  destinations: options.map((value) {
+		  	return NavigationDestination(
+				label: value.label,
+				icon: value.icon,
+				selectedIcon: value.selectedIcon,
+				tooltip: value.label
+			);
+		  }).toList(),
+	  ), // NavigationBar
+	  body: screenIndex == 0 ? index0(context) : index1(context),
+	  );
   }
 }

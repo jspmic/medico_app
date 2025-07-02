18a7dd24eb0e575729008dc0866ef55ec355069a
jspmic <micaeljaspe@gmail.com>
2025-07-02 19:13:04 +0200

    feat: added class to interact with the DB and store the results in the model

---
+++ lib/api/rdv_api.dart
@@ -0,0 +1,44 @@
+import "rdv_model.dart";
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class RdvApi {
+	Future<List<RdvModel?>> getRdv(int userID) async {
+		const host = String.fromEnvironment('HOST');
+		if (host.isEmpty) {
+		  throw AssertionError('HOST is not set');
+		}
+		Uri uri = Uri.parse("$host/rdv?id_user=$userID");
+		http.Response response = await http.get(uri);
+		if (response.statusCode == 200) {
+			List<RdvModel?> listRdv = [];
+			List body = json.decode(response.body);
+			for (String rdv in body) {
+				listRdv.add(rdvFromJson(rdv));
+			}
+			return listRdv;
+		}
+		else {
+			return [];
+		}
+	}
+	Future<bool> insertRdv(RdvModel user) async {
+		const host = String.fromEnvironment('HOST');
+		if (host.isEmpty) {
+		  throw AssertionError('HOST is not set');
+		}
+		var uri = Uri.parse("$host/user");
+		http.Response response = await http.post(
+			uri,
+			headers: <String, String>{
+			  'Content-Type': 'application/json; charset=UTF-8'
+			},
+			body: rdvToJson(user)
+		);
+		if (response.statusCode == 201) {
+			return true;
+		}
+		return false;
+	}
+}
